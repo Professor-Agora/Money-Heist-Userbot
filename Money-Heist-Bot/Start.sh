152dc947125dbde2d09fa7f1d8e500053e18dc5d
Professor Agora <95218616+mr-agoraswamy@users.noreply.github.com>
2021-12-06 18:24:39 +0530

    Create Start.sh

---
+++ Money-Heist-Bot/Start.sh
@@ -0,0 +1,24 @@
+#   Copyright (C) 2021 Ptofessor-Agora
+#
+#   Poject VΛDΣЯ is free software: 
+#   you can redistribute it and/or modify it under the terms of the GNU General Public License 
+#   as published by the Free Software Foundation, either version 3 of the License, 
+#   or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, 
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along with this program. 
+#   If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+echo """
+ BELLO CIAO BELLA CIAO BELLA CIAO
+ CIAO CIAO CIAO
+
+rm -rf Money-Heist-Bot
+git clone https://github.com/Professor-Agora/Money-Heist-bot
+cd Money-Heist-Bot
+python3 -m userbot
